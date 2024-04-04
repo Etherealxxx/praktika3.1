9ac5a379eeb2ea0c7e616c2097657a10db3d6e3f
genri <genri@LAPTOP-LOTH031F>
2024-04-04 10:37:54 +0300

    Добавьте файлы проекта.

---
+++ praktika3/AccountEF.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace praktika3
+{
+    /// <summary>
+    /// Логика взаимодействия для AccountEF.xaml
+    /// </summary>
+
+    public partial class AccountEF : Page
+    {
+        private SteamEntities1 steam = new SteamEntities1();
+        public AccountEF()
+        {
+            InitializeComponent();
+            AccountEf.ItemsSource = steam.account.ToList();
+        }
+    }
+}
---
+++ praktika3/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using praktika3.SteamDataSetTableAdapters;
+
+namespace praktika3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+         
+        accountTableAdapter account = new accountTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            AccountGrid.ItemsSource = account.GetFullInfo();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AccountGrid.Columns[0].Visibility = Visibility.Collapsed;
+            AccountGrid.Columns[1].Visibility = Visibility.Collapsed;
+            AccountGrid.Columns[2].Visibility = Visibility.Collapsed;
+            AccountGrid.Columns[3].Header = "Никнейм";
+            AccountGrid.Columns[4].Header = "Имя друга";
+            AccountGrid.Columns[5].Header = "Одиночная игра";
+            AccountGrid.Columns[6].Header = "Многопользовательская игра";
+            AccountGrid.Columns[7].Header = "Кооперативная игра";
+        }
+        private void ef_Click(object sender, RoutedEventArgs e)
+        {
+            AccountEF accountEF = new AccountEF();
+            this.Content = accountEF;
+            
+        }     
+    }
+}
